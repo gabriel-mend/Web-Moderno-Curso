080efa5c9df322f97483eab734db2d6027cc45c8
Gabriel <gabriel@Brendas-Mac-mini.local>
2019-10-09 11:36:13 -0300

    first commit

---
+++ fundamentos/organizacao.js
@@ -0,0 +1,12 @@
+// Javascript e organizado em senteças de códigos que podem ou nao terminar em ; 
+// E formado por blocos que sao delimitados por { }
+
+console.log(
+    "setença de codigo");
+
+{
+    {
+        console.log("Olá");
+        console.log('Mundo') //Padrao do curso
+    }
+}
