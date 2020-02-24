59881cfe4473369f75e1469aa622dcdba96e3463
OGabrielPereira <gabrielmendoncapereira14@gmail.com>
2020-02-24 20:47:24 -0300

    esnext

---
+++ esnext/usandoPromises.js
@@ -0,0 +1,30 @@
+const http = require('http')
+
+const getTurma = (letra, callback) => {
+    const url = `http://files.cod3r.com.br/curso-js/turma${letra}.json`
+    return new Promise((resolve, reject) => {
+        http.get(url, res => {
+            let resultado = ''
+    
+            res.on('data', dados => {
+                resultado += dados
+            })
+            res.on('end', () => {
+                try {
+                    resolve(JSON.parse(resultado))
+                } catch(e) {
+                    reject(e)
+                }
+            })
+        })
+    })
+} 
+
+
+Promise.all([getTurma('A'), getTurma('B'), getTurma('C')])
+    .then(turmas => [].concat(...turmas))
+    .then(alunos => alunos.map((a) => a.nome))
+    .then(nomes => console.log(nomes))
+    .catch(e => console.log(e.message))
+
+getTurma('D').catch(e => console.log(e.message))
